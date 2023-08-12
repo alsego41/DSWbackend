d6e2e1fe61d30118dad1c9526c222de9f85a4c3b
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-12 19:57:28 -0300

    Added some middlewares and import routes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-console.log("ts");
-console.log("holas");
+import express from "express"
+import cors from "cors"
+import routes from "./routes.js"
 
+const app = express()
+
+app.use(cors())
+// app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+    console.log(req.method);
+    console.log(req.originalUrl);
+    next()
+})
+
+app.use('/', routes)
+
+app.listen(3000, () => {
+    console.log("Listening in 3000");
+})
