7899e4fedc91d5f8144f558cd36417ba694fabd7
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-28 19:58:13 -0300

    Update schema

--- src/models/citySchema.ts
+++ src/models/citySchema.ts
@@ -2,6 +2,5 @@ import mongoose from "mongoose"
 
 export const nameCitySchema = new mongoose.Schema({ 
     nameCity:  {type: String, required: true}, 
-    fans: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Person' }],
     properties: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }]
 })
