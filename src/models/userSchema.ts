50dc42f652a6e190d222046d489da853fb6fbab8
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-28 19:56:27 -0300

    Agregada ref a propiedades del usuario

--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -8,20 +8,6 @@ export const userSchema = new mongoose.Schema({
     address:  {type: String, required: true},
     password:  {type: String, required: true},
     dob:  {type: String, required: true},
-    bankAccount:  String
+    bankAccount:  String,
+    properties: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }]
 })
-
-// const User = mongoose.model('User', userSchema)
-
-// const newUser = new User({
-//     firstName: "Pipo",
-//     lastName: "Perez",
-//     dni: 41232232,
-//     email: "pipoperez@gmail.com",
-//     address: "Corrientes 123",
-//     password: "$2b$10$Sl3w4OvQdwg8sJ8A0HXzPO2Bo6kauMqa/UxURw9RFaG9z7qo1omcC",
-//     birthDate: "1998-01-02",
-//     bankAccount: ""
-// })
-
-// await newUser.save()
