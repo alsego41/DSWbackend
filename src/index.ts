571c8729202589af9366a4d63bec4a0e487fbc70
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-06 23:04:24 -0300

    Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,34 @@
-import express from "express"
-import cors from "cors"
-import routes from "./routes.js"
-import mongoose from "mongoose"
-import "dotenv/config"
-
+import express from 'express'
+import cors from 'cors'
+import routes from './routes.js'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-    console.log(req.method);
-    console.log(req.originalUrl);
-    next()
+	console.log(req.method)
+	console.log(req.originalUrl)
+	next()
 })
 
 app.use('/', routes)
 
 app.use((req, res) => {
-    res.status(404).send("404 - Not Found")
+	res.status(404).send('404 - Not Found')
 })
 
 app.listen(3000, () => {
-    console.log("Listening in 3000");
+	console.log('Listening in 3000')
 })
 
 try {
-    await mongoose.connect(process.env.MONGO_URI || "")
-    console.log('connected to database')
+	await mongoose.connect(process.env.MONGO_URI || '')
+	console.log('connected to database')
 } catch (error) {
-    console.log(error)
+	console.log(error)
 }
--- src/models/citySchema.ts
+++ src/models/citySchema.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
-export const nameCitySchema = new mongoose.Schema({ 
-    nameCity:  {type: String, required: true}, 
-    properties: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }]
+export const nameCitySchema = new mongoose.Schema({
+	nameCity: { type: String, required: true },
+	properties: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }],
 })
