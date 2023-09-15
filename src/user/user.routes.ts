94bcc528ccfb9401407220cab7ebad3b525f61c9
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-15 00:39:20 -0300

    Update verify to sharedController

--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { userController } from './user.controller.js'
+import { SharedController } from '../shared/controller.js'
 
 export const userRouter = Router()
 
 userRouter.post(
 	'/verify',
-	userController.verifyToken,
-	userController.testTokenVerification,
+	SharedController.verifyToken,
+	SharedController.testTokenVerification,
 )
 userRouter.get('/', userController.findAll)
 userRouter.post('/login', userController.login)
@@ -14,15 +15,15 @@ userRouter.post('/register', userController.register)
 userRouter.post('/delete/:id', userController.remove)
 userRouter.patch(
 	'/properties/new',
-	userController.verifyToken,
+	SharedController.verifyToken,
 	userController.updateOwnProperties,
 )
-userRouter.patch('/edit', userController.verifyToken, userController.update)
+userRouter.patch('/edit', SharedController.verifyToken, userController.update)
 // Fixear esto, se va a la ruta de /:id cuando se hace llamada a /properties
 // Unica fix de momento, precedencia?
 userRouter.get(
 	'/properties/',
-	userController.verifyToken,
+	SharedController.verifyToken,
 	userController.populate,
 )
 userRouter.get('/:id', userController.findById)
