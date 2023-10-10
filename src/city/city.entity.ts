2f3afbc54852fa21570e4a12385499db0cf854cc
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-10-10 15:04:46 -0300

    Update index y props

--- src/city/city.entity.ts
+++ src/city/city.entity.ts
@@ -1,10 +1,16 @@
 import { Ref, getModelForClass, index, prop } from '@typegoose/typegoose'
 import { ProvinceClass } from '../province/province.entity.js'
 
-@index({ nameCity: 1, province: 1 }, { unique: true })
+@index({ nameCity: 1, nameDepartamento: 1, province: 1 }, { unique: true })
 export class CityClass {
 	@prop({ required: true })
 	public nameCity?: String
+
+	@prop({ required: true })
+	public idCity?: String
+
+	@prop({ required: true })
+	public nameDepartamento?: String
 
 	@prop({ required: true, ref: () => ProvinceClass })
 	public province?: Ref<ProvinceClass>
