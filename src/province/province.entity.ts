3e29267e4e8798b1959ee332f2cb608a468297b8
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-10-10 15:05:33 -0300

    Updated props

--- src/province/province.entity.ts
+++ src/province/province.entity.ts
@@ -3,6 +3,8 @@ import { getModelForClass, prop } from '@typegoose/typegoose'
 export class ProvinceClass {
 	@prop({ required: true, unique: true })
 	public nameProvince?: String
+	@prop({ required: true })
+	public idProvince?: String
 }
 
 export const ProvinceModel = getModelForClass(ProvinceClass)
