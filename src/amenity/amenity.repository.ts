d448459af24f6a20019ca8b4639558a48c32af65
Prhetzel <utreraedsel@outlook.com>
2023-11-11 15:49:10 -0300

    creacion de amenities

---
+++ src/amenity/amenity.repository.ts
@@ -0,0 +1,37 @@
+import { DocumentType } from '@typegoose/typegoose'
+import { Repository } from '../shared/repository'
+import {amenityclass, amenitymodel} from './amenity.entity.ts'
+
+export class amenityRepository implements Repository<amenityclass>{
+    public async findAll(): Promise<amenityclass[]> {
+     const amenity = await amenitymodel.find().exec()
+     return amenity   
+    }
+    public async findById(item: { _id: String }): Promise<amenityclass> {
+        try{
+            const amenity = await amenitymodel.findById(item._id).exec()
+            return amenity
+        } catch (err) {
+            return null
+        }
+    }
+    public async findbyname(nameAmenity: string): Promise<DocumentType<amenityclass> | null>{
+        try{
+            const amenity = await amenitymodel.findone({nameAmenity}).exec()
+            return amenity
+        } catch (err) {
+            return null
+        }
+    }
+    public async create(item: amenityclass,): Promise<DocumentType<amenityclass> | null> {
+		const newamenity = new amenitymodel(item)
+		return await newamenity.save()
+    }
+    public async update(item: {
+		_id: String
+		usertype: amenityclass
+	}): Promise<DocumentType<amenityclass> | null> {
+		const amenity = await amenitymodel.findByIdAndUpdate(item._id, item.amenity)
+		return amenity
+	}
+}
