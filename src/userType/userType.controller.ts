6142ea1b8077a128b06d08711fb7569aa93cfdf1
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-11-11 00:00:11 -0300

    fix ref a archivo class

--- src/userType/userType.controller.ts
+++ src/userType/userType.controller.ts
@@ -1,32 +1,31 @@
 import { Request, Response, NextFunction } from 'express'
 import { userTypeRepository } from './userType.repository.js'
-import { userTypeclass} from '../userType/userType.entity.js'
+import { userTypeclass } from './userType.entity.js'
 
 const repository = new userTypeRepository()
 
 async function findAll(req: Request, res: Response) {
-    const userstype = await repository.findAll()
-    if (!userstype) {
-        return res.status(404).json({message: 'No usertype found'})
-    }
-    return res.status(200).json(userstype)
+	const userstype = await repository.findAll()
+	if (!userstype) {
+		return res.status(404).json({ message: 'No usertype found' })
+	}
+	return res.status(200).json(userstype)
 }
 
-async function sanitizedInput(req: Request, res: Response, next:NextFunction) {
-    const {nametype} = req.body
-    req.body.sanitizedInput ={
-       nametype
-    }
-    next()
+async function sanitizedInput(req: Request, res: Response, next: NextFunction) {
+	const { nametype } = req.body
+	req.body.sanitizedInput = {
+		nametype,
+	}
+	next()
 }
 
 async function findById(req: Request, res: Response) {
-    console.log(req.body)
-    const usertype = await repository.findById({_id: req.params.id})
-    if (!usertype) {
-        return res.status(404).json({ messsage: 'Usertype not found'})
-    }
-    
+	console.log(req.body)
+	const usertype = await repository.findById({ _id: req.params.id })
+	if (!usertype) {
+		return res.status(404).json({ messsage: 'Usertype not found' })
+	}
 }
 
 async function remove(req: Request, res: Response) {
@@ -52,10 +51,10 @@ async function update(req: Request, res: Response) {
 		})
 }
 
-export const userTypeController ={
-    findAll,
-    findById,
-    remove,
-    update,
-    sanitizedInput,
+export const userTypeController = {
+	findAll,
+	findById,
+	remove,
+	update,
+	sanitizedInput,
 }
